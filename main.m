1492beaf58eb6b90a7b3ed1e40959b94c84e714a
Andreas Vieregg <doctorwhom042@gmail.com>
2018-01-15 22:23:50 +0100

    Aufgabe 1c) fertig

--- main.m
+++ main.m
@@ -1,1 +1,21 @@
 % RST 2 Versuch 2
+load Modellparameter.mat
+
+a = (Ke*Km)/(J * R) + mu / J;
+b = (Km * V)/(J * R);
+
+A = [0,1;0,-a];
+B = [0;b];
+C = [1,0];
+%% Aufgabe 1
+
+%c)
+sigma = ss(A,B,C,0);
+x0 = [2;-10];
+t = 0:2e-3:10;
+u = zeros (size(t));
+
+[y,t,x] = lsim(sigma, u, t, x0);
+
+plot(t,y,t, 2-10/a * (1-exp(-a*t)));
+
